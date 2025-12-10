e176fc28e3abb6dfa8c9cc64081945747ce005fc
ShameelShajaad <shameels1228@gmail.com>
2025-12-10 14:28:53 +0530

    cart items visible in cart panel

--- assets/js/placeorder.js
+++ assets/js/placeorder.js
@@ -25,13 +25,15 @@ tabs.forEach((tab) => {
 let cartCount = 0;
 
 let cartCountNo = document.querySelectorAll(".cartCount");
-let addToCartButtons = document.querySelectorAll(".menu-content button");
+let addToCartButtons = document.querySelectorAll(
+  ".menu-content button[data-id]"
+);
 
 // ADD TO CART CLICK
 addToCartButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
     cartCount++;
-    updateCart(btn)
+    updateCart(btn);
 
     // update ALL cart counters (mobile + desktop)
     cartCountNo.forEach((no) => {
@@ -59,10 +61,43 @@ cartIcon.forEach((icon) => {
 
 let cart = [];
 
-function updateCart(btn){
-  let id=btn.dataset.id
-  let name=btn.dataset.name
-  let price=btn.dataset.price
+function updateCart(btn) {
+  let id = btn.dataset.id;
+  let name = btn.dataset.name;
+  let price = parseInt(btn.dataset.price);
 
-  console.log(id+" "+name+" "+price);
+  console.log(id + " " + name + " " + price);
+
+  let item = {
+    id: id,
+    name: name,
+    price: price,
+    quantity: 1,
+  };
+
+  let existingItem = cart.find((i) => i.id === item.id);
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.push(item);
+  }
+
+  console.log(cart);
+
+  renderCart();
+}
+
+function renderCart() {
+  cartPanel.innerHTML = `<h2 class="text-xl font-bold mb-4 text-center text-yellow-400">Your Cart</h2>`;
+
+  cart.forEach((item) => {
+    let div = document.createElement("div");
+    div.className =
+      "p-2 border-b border-gray-700 flex justify-between text-white";
+    div.innerHTML = `
+    <span class="flex items-center font-semibold">${item.name} ('') ${item.quantity}</span>
+    <span class="font-bold">LKR ${item.price * item.quantity}</span>
+    `;
+    cartPanel.appendChild(div);
+  });
 }
